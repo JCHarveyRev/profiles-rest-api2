40ddffc0d305de8aec2e2684ac250ef150f10761
JC <jc.harvey.jr1@gmail.com>
2021-12-21 15:30:00 -0600

    Enable Django admin for user profile model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -12,7 +12,7 @@ class UserProfileManager(BaseUserManager):
                 raise ValueError('User must have an email address')
 
             email = self.normalize_email(email)
-            user = self.model(email=email, name=username)
+            user = self.model(email=email, name=name)
 
             user.set_password(password)
             user.save(using=self._db)
@@ -28,6 +28,7 @@ class UserProfileManager(BaseUserManager):
             user.save(using=self._db)
 
             return user
+
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
